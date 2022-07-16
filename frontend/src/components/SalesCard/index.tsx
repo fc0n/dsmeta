f3c40b3ecdf4df1631502113d57120109de4195d
fc0n <jeangabrieljb9@gmail.com>
2022-07-15 23:14:42 -0300

    div passou a ser button por acessibilidade

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -74,9 +74,9 @@ function SalesCard() {
                                     <td className="show992">{sale.deals}</td>
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
-                                        <div className="dsmeta-red-btn-container">
+                                        <button className="dsmeta-red-btn-container">
                                             <NotificationButton saleId={sale.id}/>
-                                        </div>
+                                        </button>
                                     </td>
                                 </tr>
                             )
